cd14b354c694d6c24f78fa9f6bdad183d13e7861
Florent THOMAS <florent.thomas@mind-and-go.com>
2018-01-05 17:35:12 +0100

    Deal with a correct order for inheritance

--- connector_odoo/models/__init__.py
+++ connector_odoo/models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from . import odoo_binding #Keep this order for inheritance 
 
 from . import odoo_backend
-from . import odoo_binding
 # from . import product_category 
 from . import product_uom
