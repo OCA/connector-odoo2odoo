53937c4e80f89a9be4db2e36e7e3d97baddd18d2
Florent THOMAS <florent.thomas@mind-and-go.com>
2018-02-11 16:52:38 +0100

    Override map_direct

--- connector_odoo/components/mapper.py
+++ connector_odoo/components/mapper.py
@@ -16,6 +16,33 @@ class OdooImportMapper(AbstractComponent):
         return {'backend_id': self.backend_record.id}
 
 
+    def _map_direct(self, record, from_attr, to_attr):
+        """ Apply the ``direct`` mappings.
+
+        :param record: record to convert from a source to a target
+        :param from_attr: name of the source attribute or a callable
+        :type from_attr: callable | str
+        :param to_attr: name of the target attribute
+        :type to_attr: str
+        """
+        if callable(from_attr):
+            return from_attr(self, record, to_attr)
+
+        value = record[from_attr]
+        if not value:
+            return False
+
+        # Backward compatibility: when a field is a relation, and a modifier is
+        # not used, we assume that the relation model is a binding.
+        # Use an explicit modifier external_to_m2o in the 'direct' mappings to
+        # change that.
+        field = self.model._fields[to_attr]
+        if field.type == 'many2one':
+            mapping_func = external_to_m2o(from_attr)
+            value = mapping_func(self, record, to_attr)
+        return value
+
+
 class OdooExportMapper(AbstractComponent):
     _name = 'odoo.export.mapper'
     _inherit = ['base.odoo.connector', 'base.export.mapper']
