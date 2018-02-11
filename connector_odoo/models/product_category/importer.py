16b00d1c7add80b27db1f49ae7f753ae0935a39d
Florent THOMAS <florent.thomas@mind-and-go.com>
2018-02-11 16:52:38 +0100

    Override map_direct

--- connector_odoo/models/product_category/importer.py
+++ connector_odoo/models/product_category/importer.py
@@ -68,18 +68,11 @@ class ProductCategoryImportMapper(Component):
     _inherit = 'odoo.import.mapper'
     _apply_on = 'odoo.product.category'
 
-#     direct = [
-# #         ('name', 'name'),
-#         ('type', 'type'),
-#     ]
+    direct = [
+        ('name', 'name'),
+        ('type', 'type'),
+    ]
 
-    @mapping
-    def name(self, record):
-        return {'name': record['name']}
-
-    @mapping
-    def type(self, record):
-        return {'type': record['type']}
 
     @only_create
     @mapping
